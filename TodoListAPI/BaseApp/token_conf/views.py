964d380272a7440072125f61cf7db8ae9e029d66
Manuel Bayona <manudesarrolla07@gmail.com>
2022-12-04 15:56:51 -0400

    Delete TodoListAPI directory

--- TodoListAPI/BaseApp/token_conf/views.py
+++
@@ -1,8 +0,0 @@
-# SimpleJWT
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-# serializers token_conf
-from .serializers import MyTokenObtainPairSerializer
-
-class MyTokenObtainPairView(TokenObtainPairView):
-    serializer_class = MyTokenObtainPairSerializer
--- TodoListAPI/UsersApp/urls.py
+++
@@ -1,9 +0,0 @@
-# django libraries
-from django.urls import path
-
-# views UsersApp
-from UsersApp import views
-
-urlpatterns = [
-    path('register/', views.UserRegistration.as_view()),
-]
