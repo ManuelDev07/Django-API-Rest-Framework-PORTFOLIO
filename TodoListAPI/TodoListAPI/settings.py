fcf5c1a89ba8799fcb0b8497c0e0a063761430b6
Manuel Bayona <manudesarrolla07@gmail.com>
2023-01-16 10:36:18 -0400

    modificado settings.py

--- TodoListAPI/TodoListAPI/settings.py
+++ TodoListAPI/TodoListAPI/settings.py
@@ -94,8 +94,8 @@ WSGI_APPLICATION = 'TodoListAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('ENGINE'),
+        'NAME': BASE_DIR / config('NAME',cast=str),
     }
 }
 
